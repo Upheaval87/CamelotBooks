dd3f86b418be6b734c6ddddd3f7239bcfa5373ef
Elvis Seyama <eseyama@kuwalasacco.mw>
2026-07-23 04:55:51 +0200

    fix: open PDF exports in new tab

--- resources/views/accounting/income-statement/index.blade.php
+++ resources/views/accounting/income-statement/index.blade.php
@@ -8,7 +8,7 @@
                 <a href="{{ route('accounting.income-statement.export-csv', request()->query()) }}" class="inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
                     {{ __('Export CSV') }}
                 </a>
-                <a href="{{ route('accounting.income-statement.export-pdf', request()->query()) }}" class="inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                <a href="{{ route('accounting.income-statement.export-pdf', request()->query()) }}" target="_blank" class="inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
                     {{ __('Export PDF') }}
                 </a>
             </div>
